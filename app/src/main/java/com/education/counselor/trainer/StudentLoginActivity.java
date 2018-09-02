cb516721af807f7fd72b927569244da791727436
AJINKYA BEDEKAR <ajinkyabedekar2@gmail.com>
2018-09-02 16:27:28 +0530

    Next Commit

--- app/src/main/java/com/education/counselor/trainer/StudentLoginActivity.java
+++ app/src/main/java/com/education/counselor/trainer/StudentLoginActivity.java
@@ -9,7 +9,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 public class StudentLoginActivity extends AppCompatActivity {
@@ -53,7 +55,7 @@ public class StudentLoginActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        //FirebaseUser currentUser = mAuth.getCurrentUser();
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
     public void login()
     {
@@ -76,17 +78,16 @@ public class StudentLoginActivity extends AppCompatActivity {
     public void reset()
     {
         String email = username.getText().toString();
-        String pass = password.getText().toString();
-        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
+            public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()) {
                     // Sign in success, update UI with the signed-in user's information
                     // FirebaseUser user = mAuth.getCurrentUser();
-                    Toast.makeText(getBaseContext(), "Authentication succeeded.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getBaseContext(), "Check Your Email", Toast.LENGTH_SHORT).show();
                 } else {
                     // If sign in fails, display a message to the user.
-                    Toast.makeText(getBaseContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getBaseContext(), "Reset Password Failed", Toast.LENGTH_SHORT).show();
                 }
             }
         });
