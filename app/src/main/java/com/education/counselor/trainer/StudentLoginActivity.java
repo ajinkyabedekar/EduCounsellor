d6b7d4a9c4857580ac8ceb24dd82b8b8bb9d3904
AJINKYA BEDEKAR <ajinkyabedekar2@gmail.com>
2018-09-02 17:23:53 +0530

    Next Commit

--- app/src/main/java/com/education/counselor/trainer/StudentLoginActivity.java
+++ app/src/main/java/com/education/counselor/trainer/StudentLoginActivity.java
@@ -9,9 +9,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 public class StudentLoginActivity extends AppCompatActivity {
