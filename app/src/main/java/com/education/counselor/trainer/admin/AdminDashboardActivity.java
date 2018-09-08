1a2b67d35ffa4f75f44b56226aabc35918495eb2
AJINKYA BEDEKAR <ajinkyabedekar2@gmail.com>
2018-09-08 11:21:05 +0530

    Next Commit

--- app/src/main/java/com/education/counselor/trainer/admin/AdminDashboardActivity.java
+++ app/src/main/java/com/education/counselor/trainer/admin/AdminDashboardActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.education.counselor.trainer.R;
 public class AdminDashboardActivity extends AppCompatActivity {
     Button add_student;
