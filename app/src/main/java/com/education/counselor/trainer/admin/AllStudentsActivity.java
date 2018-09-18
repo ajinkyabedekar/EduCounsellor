cf8febd728768e4abcc02e3aed4a3092a29aef4f
Shantanu Bahuguna <bshantanu1@gmail.com>
2018-09-18 19:38:38 +0530

    latest

--- app/src/main/java/com/education/counselor/trainer/admin/AllStudentsActivity.java
+++ app/src/main/java/com/education/counselor/trainer/admin/AllStudentsActivity.java
@@ -3,24 +3,19 @@ package com.education.counselor.trainer.admin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
 import com.education.counselor.trainer.R;
 
 public class AllStudentsActivity extends AppCompatActivity {
-    Button name_mobile;
-
+   RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_student);
-        name_mobile = findViewById(R.id.name_mobile);
-        name_mobile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getBaseContext(), StudentsAcademicsActivity.class));
-            }
-        });
+        setContentView(R.layout.activity_all_students);
+
+
     }
 }
