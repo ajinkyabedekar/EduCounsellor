1850d49c7475c97a4900abbd521c37298035c64f
AJINKYA BEDEKAR <ajinkyabedekar2@gmail.com>
2018-09-20 19:50:15 +0530

    Next Commit

--- app/src/main/java/com/education/counselor/trainer/admin/CentersListActivity.java
+++ app/src/main/java/com/education/counselor/trainer/admin/CentersListActivity.java
@@ -1,33 +1,81 @@
 package com.education.counselor.trainer.admin;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.education.counselor.trainer.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class CentersListActivity extends AppCompatActivity {
-    Button add_center, center;
+    Button add_center;
+    RecyclerView recyclerView;
+    DatabaseReference db;
+    EditText name;
+    CentersListEntryAdapter adapter;
+    ProgressBar pg;
+    Context mContext;
+    private ArrayList<CentersListEntryVo> details = new ArrayList<>();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        pg.setVisibility(View.VISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_centers_list);
         add_center = findViewById(R.id.add_center);
-        center = findViewById(R.id.center);
         add_center.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getBaseContext(), AddCentersActivity.class));
             }
         });
-        center.setOnClickListener(new View.OnClickListener() {
+        mContext = this;
+        pg = findViewById(R.id.progress);
+        recyclerView = findViewById(R.id.recycle);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        db = FirebaseDatabase.getInstance().getReference("centers");
+        pg.setVisibility(View.VISIBLE);
+        db.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getBaseContext(), CentersDetailsActivity.class));
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    CentersListEntryVo s = new CentersListEntryVo();
+                    s.setName(Objects.requireNonNull(ds.child("name").getValue()).toString());
+                    details.add(s);
+                }
+                adapter = new CentersListEntryAdapter(mContext, details);
+                pg.setVisibility(View.GONE);
+                recyclerView.setAdapter(adapter);
+                Toast.makeText(mContext, adapter.getItemCount() + " centers", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(mContext, "Error", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }
---
+++ app/src/main/java/com/education/counselor/trainer/admin/CentersListEntryVo.java
@@ -0,0 +1,17 @@
+package com.education.counselor.trainer.admin;
+
+public class CentersListEntryVo {
+    private String name;
+
+    CentersListEntryVo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
