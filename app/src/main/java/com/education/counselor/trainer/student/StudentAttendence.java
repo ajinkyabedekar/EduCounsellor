e2dc1a2dbdf3d89898dc09f4414be38f9720b163
Shantanu Bahuguna <bshantanu1@gmail.com>
2018-10-01 19:57:08 +0530

    commit

---
+++ app/src/main/java/com/education/counselor/trainer/student/StudentAttendence.java
@@ -0,0 +1,15 @@
+package com.education.counselor.trainer.student;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.education.counselor.trainer.R;
+
+public class StudentAttendence extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_attendence);
+    }
+}
