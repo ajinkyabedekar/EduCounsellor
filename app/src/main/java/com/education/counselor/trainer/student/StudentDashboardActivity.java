74e338bb53e31a2c807b3432f88fb90c5bd3855a
AJINKYA BEDEKAR <ajinkyabedekar2@gmail.com>
2018-10-02 16:44:20 +0530

    Next Commit

--- app/src/main/java/com/education/counselor/trainer/student/StudentDashboardActivity.java
+++ app/src/main/java/com/education/counselor/trainer/student/StudentDashboardActivity.java
@@ -1,8 +1,13 @@
 package com.education.counselor.trainer.student;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,8 +15,10 @@ import android.widget.Toast;
 
 import com.education.counselor.trainer.R;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class StudentDashboardActivity extends AppCompatActivity {
-    Button attend_class, add_query, query_list, courses_list, project_details, internship_list, placement_list;
+    Button attend_class, add_query, query_list, courses_list, project_details, internship_list, placement_list, call_counsellor;
     private boolean pressed = false;
 
     @Override
@@ -81,6 +88,21 @@ public class StudentDashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getBaseContext(), PlacementListActivity.class));
+            }
+        });
+        call_counsellor = findViewById(R.id.call_counsellor);
+        call_counsellor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(StudentDashboardActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getBaseContext(), "Please Provide Permissions", Toast.LENGTH_SHORT).show();
+                    ActivityCompat.requestPermissions(StudentDashboardActivity.this, new String[]{Manifest.permission.CALL_PHONE},1);
+                }
+                else
+                {
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "7425835169"));
+                    startActivity(intent);
+                }
             }
         });
     }
