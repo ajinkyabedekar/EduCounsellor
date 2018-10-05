4783d66c477beff4a213f45c54b73a25b17fc069
AJINKYA BEDEKAR <ajinkyabedekar2@gmail.com>
2018-10-06 03:55:09 +0530

    Next Commit

---
+++ app/src/main/java/com/education/counselor/trainer/counsellor/PlacementCentersEntryVo.java
@@ -0,0 +1,16 @@
+package com.education.counselor.trainer.counsellor;
+
+public class PlacementCentersEntryVo {
+    private String name;
+
+    PlacementCentersEntryVo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/education/counselor/trainer/counsellor/StartupCentersEntryVo.java
@@ -0,0 +1,16 @@
+package com.education.counselor.trainer.counsellor;
+
+public class StartupCentersEntryVo {
+    private String name;
+
+    StartupCentersEntryVo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
