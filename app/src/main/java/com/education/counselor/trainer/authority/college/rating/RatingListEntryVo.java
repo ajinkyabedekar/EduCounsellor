e8bff3f0bb61e73335fc1d170eef667e096e7ab3
Biren Sharma <31134009+birensharma@users.noreply.github.com>
2018-11-14 02:40:06 +0530

    next updates

---
+++ app/src/main/java/com/education/counselor/trainer/authority/college/rating/RatingListEntryVo.java
@@ -0,0 +1,18 @@
+package com.education.counselor.trainer.authority.college.rating;
+public class RatingListEntryVo {
+    private String name, rating;
+    RatingListEntryVo() {
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getRating() {
+        return rating;
+    }
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
