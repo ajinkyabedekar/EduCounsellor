f533a3df903cf72fb7938403296598e1c416ebca
Biren Sharma <31134009+birensharma@users.noreply.github.com>
2018-11-14 02:21:54 +0530

    updates

---
+++ app/src/main/java/com/education/counselor/trainer/authority/college/internship_list/InternshipListEntryVo.java
@@ -0,0 +1,12 @@
+package com.education.counselor.trainer.authority.college.internship_list;
+public class InternshipListEntryVo {
+    private String name;
+    InternshipListEntryVo() {
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
