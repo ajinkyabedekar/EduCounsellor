58f287dfe8be717eb696eaf420330261227e37f6
Biren Sharma <31134009+birensharma@users.noreply.github.com>
2018-11-14 13:53:28 +0530

    updates

---
+++ app/src/main/java/com/education/counselor/trainer/authority/college/suggestion_list/SuggestionListEntryVo.java
@@ -0,0 +1,18 @@
+package com.education.counselor.trainer.authority.college.suggestion_list;
+public class SuggestionListEntryVo {
+    private String name, phone;
+    SuggestionListEntryVo() {
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
