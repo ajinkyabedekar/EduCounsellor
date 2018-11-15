ecfc7014c99cd71d9fc9f2c9189497177b0ee46b
yognam <yogesh.sharma@st.niituniversity.in>
2018-11-15 23:08:32 +0530

    Location Parameter

--- app/src/main/java/com/education/counselor/trainer/student/attendance/LocationParameter.java
+++ app/src/main/java/com/education/counselor/trainer/student/attendance/LocationParameter.java
@@ -1,19 +1,26 @@
+
+//ADDING LOCACTION PARAMETERS
 package com.education.counselor.trainer.student.attendance;
+//LocationParameter class
 public class LocationParameter {
     private double latitude, longitude;
     LocationParameter(double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
     }
+    //getter method of latitude for retriving values
     public double getLatitude() {
         return latitude;
     }
+    //setter method of latitude for seting values
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
+        //getter method of longitude for retriving values
     public double getLongitude() {
         return longitude;
     }
+        //getter method of longitude for retriving values
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
