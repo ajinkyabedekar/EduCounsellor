389b501620995d86beeffc55953bf33350599fe7
Biren Sharma <31134009+birensharma@users.noreply.github.com>
2018-11-15 23:52:03 +0530

    next updates

---
+++ app/src/main/java/com/education/counselor/trainer/authority/college/placement_list/PlacementListActivity.java
@@ -0,0 +1,61 @@
+package com.education.counselor.trainer.authority.college.placement_list;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.education.counselor.trainer.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
+public class PlacementListActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    DatabaseReference db;
+    PlacementListEntryAdapter adapter;
+    ProgressBar pg;
+    Context mContext;
+    private ArrayList<PlacementListEntryVo> details = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_placement_list2);
+        mContext = this;
+        pg = findViewById(R.id.progress);
+        recyclerView = findViewById(R.id.recycle);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        db = FirebaseDatabase.getInstance().getReference("placements");
+        pg.setVisibility(View.VISIBLE);
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    PlacementListEntryVo s = new PlacementListEntryVo();
+                    s.setName(Objects.requireNonNull(ds.child("name").getValue()).toString());
+                    details.add(s);
+                }
+                if (details.size() == 0) {
+                    Toast.makeText(getBaseContext(), "No Placement Found", Toast.LENGTH_SHORT).show();
+                }
+                adapter = new PlacementListEntryAdapter(mContext, details);
+                pg.setVisibility(View.GONE);
+                recyclerView.setAdapter(adapter);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(mContext, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/education/counselor/trainer/authority/college/placement_list/PlacementListEntryVo.java
@@ -0,0 +1,12 @@
+package com.education.counselor.trainer.authority.college.placement_list;
+public class PlacementListEntryVo {
+    private String name;
+    PlacementListEntryVo() {
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
