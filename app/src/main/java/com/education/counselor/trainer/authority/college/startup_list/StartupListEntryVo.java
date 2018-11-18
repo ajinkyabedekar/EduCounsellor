b5b82a6c00eee7b009dadbe3cec53fee4cfe0313
Biren Sharma <31134009+birensharma@users.noreply.github.com>
2018-11-18 23:24:47 +0530

    next updates

--- app/src/main/java/com/education/counselor/trainer/authority/college/startup_list/StartupListEntryVo.java
+++ app/src/main/java/com/education/counselor/trainer/authority/college/startup_list/StartupListEntryVo.java
@@ -1,6 +1,7 @@
 package com.education.counselor.trainer.authority.college.startup_list;
 /*-------------------------------------------------------------------------------------------
- |     Its is a model class for writing data in this module                                   |
+ |     Its is a model class for writing data in this module                                  |
+ |      Author : Biren Sharma                                                                |
  |---------------------------------------------------------------------------------------------
 */
 public class StartupListEntryVo {
