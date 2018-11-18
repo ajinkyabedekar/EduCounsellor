8d84d13f221041a0b9329b54e73e8b595817612b
yognam <yogesh.sharma@st.niituniversity.in>
2018-11-18 23:58:59 +0530

    Update InternshipListEntryVo.java

--- app/src/main/java/com/education/counselor/trainer/student/internship/InternshipListEntryVo.java
+++ app/src/main/java/com/education/counselor/trainer/student/internship/InternshipListEntryVo.java
@@ -2,6 +2,9 @@
 *************************************
 *      Author:Yogesh Sharma         *
 *************************************
+******************************************************************
+* this is model class for retriving datas of coursefrom firebase *
+******************************************************************
 */
 package com.education.counselor.trainer.student.internship;
 public class InternshipListEntryVo {
