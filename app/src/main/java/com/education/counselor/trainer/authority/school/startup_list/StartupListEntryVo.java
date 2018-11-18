8f43b6ef8696d8e792bce038855511580f216b5a
Biren Sharma <31134009+birensharma@users.noreply.github.com>
2018-11-18 23:53:11 +0530

    next updates

--- app/src/main/java/com/education/counselor/trainer/authority/school/startup_list/StartupListEntryVo.java
+++ app/src/main/java/com/education/counselor/trainer/authority/school/startup_list/StartupListEntryVo.java
@@ -1,4 +1,13 @@
 package com.education.counselor.trainer.authority.school.startup_list;
+/*
+  
+******************************************************************
+* this is model class for setting and retriving datas from firebase *
+******************************************************************
+*************************************
+*      Biren Sharma         *
+*************************************
+*/
 public class StartupListEntryVo {
     private String name;
     StartupListEntryVo() {
