f7d9a4519ffebcd1a3fa9c1e7d45bc6a7cb49c98
Biren Sharma <31134009+birensharma@users.noreply.github.com>
2018-11-18 23:55:22 +0530

    next updates

--- app/src/main/java/com/education/counselor/trainer/authority/school/project_list/ProjectListEntryVo.java
+++ app/src/main/java/com/education/counselor/trainer/authority/school/project_list/ProjectListEntryVo.java
@@ -1,4 +1,13 @@
 package com.education.counselor.trainer.authority.school.project_list;
+/*
+  
+******************************************************************
+* this is model class for setting and retriving datas from firebase *
+******************************************************************
+*************************************
+*      Biren Sharma         *
+*************************************
+*/
 public class ProjectListEntryVo {
     private String name;
     ProjectListEntryVo() {
