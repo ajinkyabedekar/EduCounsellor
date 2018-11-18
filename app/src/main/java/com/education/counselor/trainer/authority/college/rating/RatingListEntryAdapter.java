a21f9bd4777b584255f28f412705dea384cb7c95
Biren Sharma <31134009+birensharma@users.noreply.github.com>
2018-11-18 23:10:34 +0530

    next update

--- app/src/main/java/com/education/counselor/trainer/authority/college/rating/RatingListEntryAdapter.java
+++ app/src/main/java/com/education/counselor/trainer/authority/college/rating/RatingListEntryAdapter.java
@@ -6,6 +6,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+/*-------------------------------------------------------------------------------------------
+ |     This file is for populating componetns of the Lists                                    |
+ |---------------------------------------------------------------------------------------------
+*/
 
 import com.education.counselor.trainer.R;
 
