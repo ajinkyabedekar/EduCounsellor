6cebd9013ea41b526daa239dd26cf94ec17a7f92
yognam <yogesh.sharma@st.niituniversity.in>
2018-11-19 01:13:28 +0530

    Update QueryListActivity.java

--- app/src/main/java/com/education/counselor/trainer/student/query/QueryListActivity.java
+++ app/src/main/java/com/education/counselor/trainer/student/query/QueryListActivity.java
@@ -2,6 +2,10 @@
 *************************************
 *      Author:Yogesh Sharma         *
 *************************************
+ -------------------------------------------------------------------------------------------
+|     Indevidual component of the Query  list  related with students are listed here        |
+ --------------------------------------------------------------------------------------------
+
 */
 package com.education.counselor.trainer.student.query;
 
