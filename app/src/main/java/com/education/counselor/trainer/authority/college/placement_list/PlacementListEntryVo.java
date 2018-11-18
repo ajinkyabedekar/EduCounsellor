27879cd3fcad63d5c057419d910a4db1c4cd987b
Biren Sharma <31134009+birensharma@users.noreply.github.com>
2018-11-18 23:45:22 +0530

    next updates

--- app/src/main/java/com/education/counselor/trainer/authority/college/placement_list/PlacementListEntryVo.java
+++ app/src/main/java/com/education/counselor/trainer/authority/college/placement_list/PlacementListEntryVo.java
@@ -1,4 +1,14 @@
 package com.education.counselor.trainer.authority.college.placement_list;
+/*
+
+  
+******************************************************************
+* this is model class for retriving datas of coursefrom firebase *
+******************************************************************
+*************************************
+*      Biren Sharma         *
+*************************************
+*/
 public class PlacementListEntryVo {
     private String name;
     PlacementListEntryVo() {
